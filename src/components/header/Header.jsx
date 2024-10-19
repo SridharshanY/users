b85599eae77c10fe428d98241c0a97ab02479b73
SridharshanY <sridharshany.2001@gmail.com>
2024-10-19 12:48:36 +0530

    started for the day

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,6 +4,7 @@ import './Header.css'
 const Header = ({ user }) => {
   return (
     <div className="headerWrapper">
+      <img src={user?.picture?.medium} alt="" />
       <p>
         {user?.name?.first} {user?.name?.last}
       </p>
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -6,7 +6,17 @@ const Main = ({name, picture, email, phone, location, registered}) => {
     <div className='cardWrapper'>
       <div className="cardMain">
         <div className="cardHeader">
-          
+          <img src={picture?.large} alt="" />
+          <h1>{name?.first} {name?.last}</h1>
+        </div>
+        <div className="cardMiddle">
+          <h3>{email}</h3>
+          <h3>{phone}</h3>
+          <h3>{location?.city}, {location?.country}</h3>
+        </div>
+        <div className="cardFooter">
+          <p>Age {registered?.age}</p>
+          <p>{new Date(registered?.date).toLocaleDateString()}</p>
         </div>
       </div>
     </div>
